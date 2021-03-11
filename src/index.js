cf200a6df50a4e05f2cc9934770d744543d84281
TouHouPolice <43275328+TouHouPolice@users.noreply.github.com>
2021-03-11 09:57:37 +0800

    styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { run } from "./app/app";
 import { AlertService } from "./app/alert.service";
 import { ComponentService } from "./app/component.service";
-import "./main.scss";
+import "./styles/main.scss";
 const alertService = new AlertService();
 const componentService = new ComponentService();
 run(alertService, componentService);
